3898e91508dfb1d1b7292d5fed3b6d2aa76c0d2e
nuruleezzh <106294191+nuruleezzh@users.noreply.github.com>
2022-05-27 17:29:55 +0800

    Update and rename Screen 1 to Screen1.java

---
+++ Screen1.java
@@ -0,0 +1,16 @@
+Author: Nurul Izzah Binti Hazemi
+Date:   Fri May 27 
+
+public String getName() {
+  return name;
+}
+public void setName(String name) {
+  this.name = name;
+}
+public String getNumber() {
+  return number;
+}
+public void setNumber(String number){
+  this.number = number;
+}
+}
